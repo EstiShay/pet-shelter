e39c4304fc2eabdb1e5889e5f202776f6e866913
Esti Shay and Kate Mashokha <esti@>
2017-07-24 10:58:16 -0700

    add UI logic to create animal constructor from form

--- js/scripts.js
+++ js/scripts.js
@@ -8,34 +8,52 @@ function Pet (petName, petPic, age, animalType) {
   this.adoptionStatus = true;
 }
 
-//adoptionStatus will be reset using checkbox on details display
-Pet.prototype.adopt = function(newStatus) {
-  this.adoptionStatus = newStatus;
-}
-
-//Push individual pets to this array
-function Shelter() {
-  this.pets = [];
-}
-
-Shelter.prototype.filterAdopted = function() {
-  adoptedPets = [];
-  this.pets.forEach(function(pet) {
-    if (this.adoptionStatus === false) {
-      adoptedPets.push(pet);
-    }
-  });
-  return adoptedPets;
-}
-
-Shelter.prototype.filterNotAdopted = function() {
-  notAdoptedPets = [];
-  this.pets.forEach(function(pet) {
-    if (this.adoptionStatus === true) {
-      notAdoptedPets.push(pet);
-  });
-  return notAdoptedPets;
-}
+// //adoptionStatus will be reset using checkbox on details display
+// Pet.prototype.adopt = function(newStatus) {
+//   this.adoptionStatus = newStatus;
+// }
+//
+// //Push individual pets to this array
+// function Shelter() {
+//   this.pets = [];
+// }
+//
+// Shelter.prototype.filterAdopted = function() {
+//   adoptedPets = [];
+//   this.pets.forEach(function(pet) {
+//     if (this.adoptionStatus === false) {
+//       adoptedPets.push(pet);
+//     }
+//   });
+//   return adoptedPets;
+// }
+//
+// Shelter.prototype.filterNotAdopted = function() {
+//   notAdoptedPets = [];
+//   this.pets.forEach(function(pet) {
+//     if (this.adoptionStatus === true) {
+//       notAdoptedPets.push(pet);
+//   });
+//   return notAdoptedPets;
+// }
 
 
 //UI logic
+$(document).ready(function() {
+
+  $("#animalinput").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var picInput = $("input#picture").val();
+    var ageInput = parseInt($("input#age").val());
+    var animalTypeInput = $("input#animalType").val();
+
+    var newPet = new Pet(nameInput, picInput, ageInput, animalTypeInput);
+
+    $("#animaloutput").append(newPet.petName);
+    $("#animaloutput").append(newPet.petPic);
+    $("#animaloutput").append(newPet.age);
+    $("#animaloutput").append(newPet.animalType);
+
+    event.preventDefault();
+  });
+});
