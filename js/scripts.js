3252b224a337573097a62570540ccf664ad2f0d8
Esti Shay and Kate Mashokha <esti@>
2017-07-24 17:01:08 -0700

    fix Adopt Me button

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,73 @@ function Pet (petName, petPic, age, animalType) {
   this.petPic = petPic;
   this.age = age;
   this.animalType = animalType;
-  this.adoptionStatus = false;
+  this.adoptionStatus = true;
 }
 
-// //adoptionStatus will be reset using checkbox on details display
-// Pet.prototype.adopt = function(newStatus) {
-//   this.adoptionStatus = newStatus;
-// }
-//
 //Push individual pets to this array
 function Shelter() {
   this.pets = [];
 }
-//
+
+Pet.prototype.adopt = function() {
+  this.adoptionStatus = false;
+}
+
+Shelter.prototype.filterAdopted = function() {
+  var adoptedPets = [];
+  this.pets.forEach(function(pet) {
+    if (pet.adoptionStatus === false) {
+      adoptedPets.push(pet);
+    }
+  });
+  return adoptedPets;
+}
+
+
 // Shelter.prototype.filterAdopted = function() {
 //   adoptedPets = [];
 //   this.pets.forEach(function(pet) {
-//     if (this.adoptionStatus === false) {
+//     if (pet.adoptionStatus === false) {
 //       adoptedPets.push(pet);
 //     }
 //   });
 //   return adoptedPets;
 // }
-//
-// Shelter.prototype.filterNotAdopted = function() {
-//   notAdoptedPets = [];
-//   this.pets.forEach(function(pet) {
-//     if (this.adoptionStatus === true) {
-//       notAdoptedPets.push(pet);
-//   });
-//   return notAdoptedPets;
-// }
+
+
+
+
+Shelter.prototype.filterNotAdopted = function() {
+  notAdoptedPets = [];
+  this.pets.forEach(function(pet) {
+    if (pet.adoptionStatus === true) {
+      notAdoptedPets.push(pet);
+    }
+  });
+  return notAdoptedPets;
+}
+
+
+// Show the added pets
+function appendPets(newPet) {
+$(".wrapper").append('<div class="panel panel">' +
+  '<div class="panel-heading">' +
+    '<h4>' + newPet.petName + '</h4>' +
+  '</div>' +
+  '<div class="panel-body">' +
+    '<img src="' + newPet.petPic + '" alt="' + newPet.animalType + '">' +
+    '<div class="detail-reveal">' +
+      '<p>Details</p>' +
+      '<div class="details">' +
+        '<p>Age: ' + newPet.age + '</p>' +
+        '<p>Animal: ' + newPet.animalType + '</p>' +
+      // '<p class="hide-details">Hide</p>' +
+      '</div>' +
+    '</div>' +
+      '<input id="' + newPet.petName + '" class="adopt-button" type="button" name="adoption-status" value="Adopt Me">' +
+  '</div>' +
+'</div>');
+};
 
 
 //UI logic
@@ -50,35 +86,24 @@ $(document).ready(function() {
     var animalTypeInput = $("input#animaltype").val();
 
     var newPet = new Pet(nameInput, picInput, ageInput, animalTypeInput);
-    console.log(newPet);
-    console.log(newShelter);
-    console.log(newShelter.pets);
     newShelter.pets.push(newPet);
-    console.log(newShelter.pets);
+    appendPets(newPet);
 
-    $(".wrapper").append('<div class="panel panel">' +
-      '<div class="panel-heading">' +
-        '<h4>' + newPet.petName + '</h4>' +
-      '</div>' +
-      '<div class="panel-body">' +
-        '<img src="' + newPet.petPic + '" alt="' + newPet.animalType + '">' +
-        '<div class="detail-reveal">' +
-          '<p>Details</p>' +
-          '<div class="details">' +
-            '<p>Age: ' + newPet.age + '</p>' +
-            '<p>Animal: ' + newPet.animalType + '</p>' +
-          // '<p class="hide-details">Hide</p>' +
-          '</div>' +
-        '</div>' +
-          '<input class="adopt-button" type="button" name="adoption-status" value="Adopt Me">' +
-      '</div>' +
-    '</div>');
 
-    $(".adopt-button").click(function(event) {
-      this.adoptionStatus = true;
+    $(".adopt-button").click(function() {
+      var names = $(this)[0].id;
+
+      newShelter.pets.forEach(function(pet) {
+
+        if (pet.petName === names) {
+          console.log(pet.adoptionStatus);
+          pet.adopt();
+        }
+        console.log(pet.adoptionStatus);
+      });
     });
 
-    $(".detail-reveal").click(function(event) {
+    $(".detail-reveal").click(function() {
       $(this).children(".details").show();
       event.preventDefault();
     });
@@ -87,6 +112,19 @@ $(document).ready(function() {
     //   $(this).siblings().hide();
     //   event.preventDefault();
     // });
+
+    $("#adopted").click(function(event){
+      $(".wrapper").empty();
+      var filteredA = newShelter.pets.filter(function(pet) {
+        return pet.adoptionStatus == false;
+      });
+
+      alert(filteredA);
+      alert(filteredA.length);
+      filteredA.forEach(function(pet){
+        appendPets(pet);
+      });
+    });
 
     $("input#name").val('');
     $("input#picture").val('');
